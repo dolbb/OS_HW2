5dddb1f70974048f07ebf5e3219312f13da44bf1
natyLev <naty114@gmail.com>
2016-12-11 18:21:58 +0200

    יישור קו

--- sharedFolderHW2/WrappersTest.c
+++ sharedFolderHW2/WrappersTest.c
@@ -67,25 +67,47 @@ int main()
 	cout << "I will now resched as a SHORT process" << endl;
 	my_sched_param param;
 	param.sched_priority = -1;
-	param.requested_time = 1234;
+	param.requested_time = 2500;
 	assert(sched_setscheduler(getpid(), SCHED_SHORT, (sched_param*)(&param)) == 0);
 	
 	testShort();
-	
-	//cout << "Entering sched_yield" << endl;
-	//sched_yield();
-	//cout << "Back from sched_yield" << endl;
-	
-	//fork();
-	//printf("Alive...\n");
 	
 	int is_short_flag = is_short(getpid());
 	assert(is_short_flag == 1);
 	int remaining_time = short_remaining_time(getpid());
 	printf("DEBUG: is_short_flag=%d, remaining_time=%d\n", is_short_flag, remaining_time);
 	
-	bool reset = 0;
+	// Become overdue, and print remaining time (when it changes)
 	while (is_short_flag == 1) {
+		
+		// Get current state
+		is_short_flag = is_short(getpid());
+		int tmp_remaining_time = short_remaining_time(getpid());
+		
+		// Print status when timeslice changes
+		if (remaining_time != tmp_remaining_time){
+			remaining_time = tmp_remaining_time;
+			printf("DEBUG: is_short_flag=%d, remaining_time=%d\n", is_short_flag, tmp_remaining_time);
+		}	
+	}
+	
+	cout << "I am an OVERDUE process" << endl;
+	//testOverdue();
+	
+	while (remaining_time > 10) {
+		
+		// Get current state
+		is_short_flag = is_short(getpid());
+		int tmp_remaining_time = short_remaining_time(getpid());
+		
+		// Print status when timeslice changes
+		if (remaining_time != tmp_remaining_time){
+			remaining_time = tmp_remaining_time;
+			printf("DEBUG: is_short_flag=%d, remaining_time=%d\n", is_short_flag, tmp_remaining_time);
+		}
+	}
+	
+	while (remaining_time < 11){
 		
 		// Get current state
 		is_short_flag = is_short(getpid());
@@ -97,28 +119,8 @@ int main()
 			printf("DEBUG: is_short_flag=%d, remaining_time=%d\n", is_short_flag, tmp_remaining_time);
 		}
 		
-		// Busy wait
-		for (int i = 0; i < 100; i++)
-		{
-		}
-		
-		
-		// Exit before becoming overdue
-		/*if (remaining_time < tmp_remaining_time){
-			if (reset == 0) {
-				reset = 1;
-			}else {
-				printf("DEBUG: is_short_flag=%d, tmp_remaining_time=%d\n", is_short_flag, tmp_remaining_time);
-				printf("Exiting...\n");
-				exit(0);
-			}
-		}*/
-		
-
 	}
-	
-	// Become overdue
-	//testOverdue();
+
 	
 	// Become overdue
 	
